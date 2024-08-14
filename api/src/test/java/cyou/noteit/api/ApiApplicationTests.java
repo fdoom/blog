2cafd7efbf8f659fb46d43d077929cf74f68cba9
fdoom <11jjklol@naver.com>
2024-08-14 10:56:51 +0900

    init: first push

---
+++ api/src/test/java/cyou/noteit/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cyou.noteit.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
