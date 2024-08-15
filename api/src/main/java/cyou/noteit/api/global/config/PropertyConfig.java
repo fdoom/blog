372cba6801091d5cf5056f78c0a031f899ecc006
fdoom <11jjklol@naver.com>
2024-08-15 17:00:11 +0900

    feat: jdbc 연결

---
+++ api/src/main/java/cyou/noteit/api/global/config/PropertyConfig.java
@@ -0,0 +1,12 @@
+package cyou.noteit.api.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:properties/env.yml")
+})
+public class PropertyConfig {
+}
